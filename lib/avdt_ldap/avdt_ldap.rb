a6efea6d3c700c5975352dd83fa1df248ce4c148
Alessandro Verlato <averlato@gmail.com>
2011-06-22 12:25:34 +0200

    Improved documentation

--- lib/avdt_ldap/avdt_ldap.rb
+++ lib/avdt_ldap/avdt_ldap.rb
@@ -1,23 +1,29 @@
 # AvdtLdap
 
-# This gem supports LDAP authentication both on sigle and multiple servers
+# This gem supports LDAP authentication both on sigle and multiple LDAP servers
 # with a minimal configuration.
-# It requires 'net/ldap' gem.
-# 
+# It requires 'net-ldap' gem.
+#
 # USAGE
-# Single directory authentication:
-# Autentication attempt will be made on environment-specific directory (i.e "development")
-# 
+#
+# Authentication
+#
+# To verify user's credentials on ALL the specified directories (default) simply do this:
+#
 # AvdtLdap.new.valid?(login, password)
-# => true (false)
 #
-# Multiple directories authentication:
-# Here we have authentication attemps made on 2 directories: the "foobar" and
-# the default (i.e environment-specific one)
+# As mentioned this will try to authenticate the user on all the directories specified on ldap.yml and will return true or false.
+# If authentication fails an error message, containing directory response (error message and code), will be displayed on server's logs.
 #
-#  a = AvdtLdap.new(:directories => [:foobar], :include_default => true)
-#  a.valid?(login,password)
-#  => true (false)
+# Authentication only on specified directories
+#
+# If you have to check user's credentials only on some specific directories, you can pass an hash to AvdtLdap.new(), specifying on which to do the check.
+#
+# a = AvdtLdap.new(:directories => [:dir1,dir3])
+# a.valid?(login,password)
+# => true   (false)
+#
+# NOTE: The authentication process stops as soon as one positive match is found, so it's possible that not all the directories are queried.
 #
 # User's attributes access:
 # If you have to access (read) user's attributes from the directory you can
